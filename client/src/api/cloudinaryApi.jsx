c61e4b5d3e5a4ab95a96af35194c75f02700ef0a
Lily <lilymaria044@gmail.com>
2026-01-22 16:44:16 +0200

    fix

--- client/src/api/cloudinaryApi.jsx
+++ client/src/api/cloudinaryApi.jsx
@@ -1,29 +1,29 @@
 export async function uploadToCloudinary(file) {
-  const data = new FormData();
-  data.append("file", file);
-  data.append("upload_preset", "userPhoto");
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "userPhoto");
 
-  try {
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/dfnja74fz/raw/upload",
-      {
-        method: "POST",
-        body: data,
-      }
-    );
+    try {
+        const res = await fetch(
+            "https://api.cloudinary.com/v1_1/dfnja74fz/raw/upload",
+            {
+                method: "POST",
+                body: data,
+            },
+        );
+        const json = await res.json();
 
-    if (!res.ok) {
-      const errorData = await res.json();
-      throw new Error(errorData.error.message || "Unknown error occurred during upload.");
+        if (!res.ok) {
+            throw new Error(
+                json.error?.message || "Unknown error occurred during upload.",
+            );
+        }
+
+        return json.secure_url;
+    } catch (error) {
+        console.error("Upload failed:", error.message);
+        return null;
     }
-
-    const json = await res.json();
-    return json.secure_url; 
-
-  } catch (error) {
-    console.error("Upload failed:", error.message);
-    return null; 
-  }
 }
 
 
