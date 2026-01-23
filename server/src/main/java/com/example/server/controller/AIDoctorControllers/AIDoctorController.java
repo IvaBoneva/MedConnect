ab9693d2f116a3686567d750011560db8c5032f6
Lily <lilymaria044@gmail.com>
2026-01-23 18:01:15 +0200

    add new method for ai chat

--- server/src/main/java/com/example/server/controller/AIDoctorControllers/AIDoctorController.java
+++ server/src/main/java/com/example/server/controller/AIDoctorControllers/AIDoctorController.java
@@ -1,7 +1,10 @@
 package com.example.server.controller.AIDoctorControllers;
 
 
+import com.example.server.dto.GeminiDTO.AIChatRequestDTO;
+import com.example.server.service.AIDoctorServices.AIDoctorOrchestratorService;
 import com.example.server.service.AIDoctorServices.AIDoctorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +12,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/aiDoctor")
+@RequiredArgsConstructor
+
 public class AIDoctorController {
 
     private final AIDoctorService aiDoctorService;
-
-    public AIDoctorController(AIDoctorService aiDoctorService){
-        this.aiDoctorService = aiDoctorService;
-    }
+    private final AIDoctorOrchestratorService orchestrator;
 
     @PostMapping("/callGemini")
     public ResponseEntity<?> callGemini(@RequestBody String userInputText) {
 
         return aiDoctorService.callGeminiDoctor(userInputText);
+    }
+
+    @PostMapping("/chat")
+    public ResponseEntity<?> chat(@RequestBody AIChatRequestDTO request) {
+
+        Object response = orchestrator.handleUserMessage(
+                request.getConversationId(),
+                request.getMessage()
+        );
+
+        return ResponseEntity.ok(response);
     }
 
 }
